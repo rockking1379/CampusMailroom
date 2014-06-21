bd3b5463ef8f53ae7c7f30734708a55e50b33e51
James <sitzja@grizzlies.adams.edu>
2014-06-20 18:27:32 -0600

    finalizing some parts
    the logger is functioning now. had some small issues. the Updater was
    rewritten to work with a second file that will specify what files need
    downloaded. updated the gitignore to ignore a Logs directory.

--- src/com/mailroom/common/Logger.java
+++ src/com/mailroom/common/Logger.java
@@ -2,6 +2,9 @@ package com.mailroom.common;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
 import java.sql.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -29,19 +32,28 @@ public class Logger
 	 */
 	public static boolean log(Exception ex)
 	{
+		File dir = new File("./Logs");
+		
+		if(!dir.exists())
+		{
+			dir.mkdir();
+		}
+		
 		boolean retVal = true;
 		
 		Date d = new Date();
 		String sDate = new SimpleDateFormat("yyyy-MM-dd").format(d).toString();
-		File f = new File("./Logs/" + sDate + ".err");
+		String fileName = "./Logs/" + sDate + ".err";
+		File f = new File(fileName);
 		
 		try
 		{
-			Class.forName("com.sqlite.JDBC");
+			Class.forName("org.sqlite.JDBC");
 		}
 		catch (ClassNotFoundException e)
 		{
 			System.err.println("Logging Error");
+			e.printStackTrace();
 			retVal = false;
 		}
 		
@@ -51,10 +63,10 @@ public class Logger
 			{
 				f.createNewFile();
 				
-				Connection con = DriverManager.getConnection("jdbc:sqlite:" + f.getAbsolutePath());
+				Connection con = DriverManager.getConnection("jdbc:sqlite:" + fileName);
 				Statement stmnt = con.createStatement();
 				
-				stmnt.executeQuery(create);
+				stmnt.execute(create);
 				
 				stmnt.close();
 				con.close();
@@ -62,29 +74,41 @@ public class Logger
 			catch (IOException | SQLException e)
 			{
 				System.err.println("Logging Error");
+				e.printStackTrace();
 				retVal = false;
 			}
 		}
 		
 		try
 		{
-			Connection con = DriverManager.getConnection("jdbc:sqlite:" + f.getAbsolutePath());
-			java.sql.PreparedStatement stmnt = con.prepareStatement("insert into Error(error_messsage, error_stacktrace) values(?,?)");
+			Connection con = DriverManager.getConnection("jdbc:sqlite:" + fileName);
+			java.sql.PreparedStatement stmnt = con.prepareStatement("insert into Error(error_message, error_stacktrace) values(?,?)");
 			
 			stmnt.setString(1, ex.getMessage());
-			stmnt.setString(2, ex.getStackTrace().toString());
+			Writer stackTrace = new StringWriter();
+			PrintWriter pWriter = new PrintWriter(stackTrace);
+			ex.printStackTrace(pWriter);
+			stmnt.setString(2, stackTrace.toString());
 			
 			if(!stmnt.execute())
 			{
 				retVal = false;
 			}
-			
+			pWriter.close();
+			stackTrace.close();
 			stmnt.close();
 			con.close();
 		}
 		catch (SQLException e)
 		{
 			System.err.println("Logging Error");
+			e.printStackTrace();
+			retVal = false;
+		}
+		catch (IOException e)
+		{
+			System.err.println("Logging Erorr");
+			e.printStackTrace();
 			retVal = false;
 		}
 		
--- src/com/mailroom/updater/UpdaterController.java
+++ src/com/mailroom/updater/UpdaterController.java
@@ -18,9 +18,9 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-import com.mailroom.mainclient.MainFrame;
 import com.panemu.tiwulfx.dialog.MessageDialog;
 import com.panemu.tiwulfx.dialog.MessageDialogBuilder;
+import com.mailroom.common.Logger;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -56,116 +56,203 @@ public class UpdaterController implements Initializable
 	{
 	}
 	
+	/**
+	 * Action Handler for Update Button
+	 * @param ae Action Event passed by Button
+	 */
 	public void btnUpdateAction(ActionEvent ae)
 	{		
 		pindicatorProgress.setProgress(-1);
 		lblDownload.setVisible(true);
 		lblWaiting.setVisible(true);
+		lblFinished.setVisible(false);
+		btnExit.setVisible(false);
 		
 		new updater();
 	}
 	
+	/**
+	 * Action Handler for Exit Button
+	 * @param ae Action Event passed by Button
+	 */
 	public void btnExitAction(ActionEvent ae)
 	{
 		pindicatorProgress.setProgress(1);
 		System.exit(1);
 	}
 	
+	/**
+	 * Handles all the updating logic
+	 * <br>
+	 * Spins off to sperate thread so it doesnt disrupt UI
+	 * @author James
+	 *
+	 */
 	private class updater implements Runnable
 	{
 		private Thread t;
 		
+		/**
+		 * Constructor
+		 */
 		public updater()
 		{
 			t = new Thread(this);
 			t.start();
 		}
 		
+		/**
+		 * Main Updater Method
+		 * <br>
+		 * Where all real logic happens
+		 */
 		@Override
 		public void run()
 		{
 			try
 			{
 				properties = new Properties();
-				File propFile = new File("./configuration.properties");
+				File prop = new File("./configuration.properties");
 				
-				if(propFile.exists())
+				if(prop.exists())
 				{
-					FileInputStream file = new FileInputStream(propFile);
-					properties.load(file);
+					//Load the Configuration
+					FileInputStream fStream = new FileInputStream(prop);
+					properties.load(fStream);
 					
-					file.close();
-					
+					//Once done, move on to remote file retrieval/processing
 					try
 					{
 						URL url = new URL("http://minecraft.math.adams.edu/ptracker/version.php");
 						HttpURLConnection con = (HttpURLConnection) url.openConnection();
 						con.setRequestMethod("GET");
-						con.setRequestProperty("Accept",  "txt/plain");
+						con.setRequestProperty("Accept", "txt/plain");
 						con.connect();
 						
-						int statusCode = con.getResponseCode();
+						int response = con.getResponseCode();
 						
-						if(statusCode == HttpURLConnection.HTTP_OK)
+						//used to make it easy to handle other response codes
+						//only one really care about is OK(200)
+						switch(response)
 						{
-							InputStreamReader isr = new InputStreamReader(con.getInputStream());
-							BufferedReader br = new BufferedReader(isr);
-							String json = br.readLine();
-							
-							JSONParser parser = new JSONParser();
-							
-							Object obj = parser.parse(json);
-							JSONObject version = (JSONObject) obj;
-							
-							String availVersion = version.get("major") + "." + version.get("minor") + "." + version.get("revision");
-							br.close();
-							isr.close();
-							con.disconnect();
-							
-							URL mainclient = new URL("http://minecraft.math.adams.edu/ptracker/" + availVersion + "/MainClient.jar");
-							ReadableByteChannel rbc = Channels.newChannel(mainclient.openStream());
-							FileOutputStream fos = new FileOutputStream("./MainClient.jar");
-							fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
-							fos.close();
-							rbc.close();
-							
-							URL otherclient = new URL("http://minecraft.math.adams.edu/ptracker/" + availVersion + "/OtherClient.jar");
-							rbc = Channels.newChannel(otherclient.openStream());
-							fos = new FileOutputStream("./OtherClient.jar");
-							fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
-							fos.close();
-							rbc.close();
-							
-							properties.setProperty("VERSION", availVersion);
-							properties.setProperty("BUILD", version.get("build").toString());
-							
-							try
+							case HttpURLConnection.HTTP_OK:
 							{
-								FileOutputStream oStream = new FileOutputStream(propFile);
+								InputStreamReader isr = new InputStreamReader(con.getInputStream());
+								BufferedReader br = new BufferedReader(isr);
+								String json = br.readLine();
+								
+								JSONParser parser = new JSONParser();
+								
+								Object obj = parser.parse(json);
+								JSONObject version = (JSONObject) obj;
+								
+								String availVersion = version.get("major") + "." + version.get("minor") + "." + version.get("revision");
+								br.close();
+								isr.close();
+								con.disconnect();
+								
+								url = new URL("http://minecraft.math.adams.edu/ptracker/" + availVersion + "/files.php");
+								con = (HttpURLConnection) url.openConnection();
+								con.setRequestMethod("GET");
+								con.setRequestProperty("Accept", "txt/plain");
+								con.connect();
+								
+								isr = new InputStreamReader(con.getInputStream());
+								br = new BufferedReader(isr);
+								json = br.readLine();
+								
+								obj = parser.parse(json);
+								JSONObject files = (JSONObject) obj;
+								
+								int fileCount = Integer.valueOf(files.get("filecount").toString());
+								
+								for(int i=0; i<fileCount; i++)
+								{
+									String fileName = files.get(String.valueOf(i)).toString();
+									URL jar = new URL("http://minecraft.math.adams.edu/ptracker/" + availVersion + "/" + fileName);									
+									ReadableByteChannel rbc = Channels.newChannel(jar.openStream());
+									FileOutputStream fos = new FileOutputStream("./" + fileName);
+									fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+									fos.close();
+									rbc.close();
+								}
+								
+								properties.setProperty("VERSION", availVersion);
+								properties.setProperty("BUILD", version.get("build").toString());
+								
+								FileOutputStream oStream = new FileOutputStream(prop);
 								properties.store(oStream, "System Configuration");
 								oStream.close();
+								
+								try
+								{
+									lblDownload.setVisible(false);
+								}
+								catch(IllegalStateException ise)
+								{
+									Logger.log(ise);
+								}
+								try
+								{
+									lblWaiting.setVisible(false);
+								}
+								catch(IllegalStateException ise)
+								{
+									Logger.log(ise);
+								}
+								try
+								{
+									lblFinished.setVisible(true);
+								}
+								catch(IllegalStateException ise)
+								{
+									Logger.log(ise);
+								}
+								try
+								{
+									pindicatorProgress.setVisible(false);
+								}
+								catch(IllegalStateException ise)
+								{
+									Logger.log(ise);
+								}
+								try
+								{	
+									btnExit.setVisible(true);
+								}
+								catch(IllegalStateException ise)
+								{
+									Logger.log(ise);
+								}
+								try
+								{
+									btnUpdate.setVisible(false);
+								}
+								catch(IllegalStateException ise)
+								{
+									Logger.log(ise);
+								}
+								break;
 							}
-							catch(IOException e)
+							case HttpURLConnection.HTTP_NOT_FOUND:
 							{
-								System.err.println("Error: " + e.getMessage());
+								MessageDialogBuilder.error().message("Error Connecting to Update Server").buttonType(MessageDialog.ButtonType.OK).show(null);
+								System.exit(-1);
+								break;
 							}
-						}
-						else
-						{
-							MessageDialogBuilder.error().message("Error Reaching Update Server").title("Error").buttonType(MessageDialog.ButtonType.OK).show(MainFrame.stage.getScene().getWindow());
 						}
 					}
-					catch (MalformedURLException e)
+					catch(MalformedURLException e)
 					{
-						System.err.println("Error: " + e.getMessage());
+						Logger.log(e);
 					}
 					catch(IOException e)
 					{
-						System.err.println("Error: " + e.getMessage());
+						Logger.log(e);
 					}
-					catch (ParseException e)
+					catch(ParseException e)
 					{
-						System.err.println("Error: " + e.getMessage());
+						Logger.log(e);
 					}
 				}
 				else
@@ -176,15 +263,9 @@ public class UpdaterController implements Initializable
 			}
 			catch(IOException e)
 			{
-				System.err.println("Error: " + e.getMessage());
+				Logger.log(e);
 				System.exit(-1);
 			}
-			
-			lblDownload.setVisible(false);
-			lblWaiting.setVisible(false);
-			lblFinished.setVisible(true);
-			pindicatorProgress.setVisible(false);
-			btnExit.setVisible(true);
 		}
 	}
 
